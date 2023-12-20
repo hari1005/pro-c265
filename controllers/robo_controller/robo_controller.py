74d3bdf1d48551c1c88c32368412363782e1fd9e
hari1005 <62102842+hari1005@users.noreply.github.com>
2023-12-20 18:53:10 +0530

    Add files via upload

---
+++ controllers/robo_controller/robo_controller.py
@@ -0,0 +1,33 @@
+"""robo_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot,Keyboard,Motion
+
+# create the Robot instance.
+robot = Robot()
+keyboard=Keyboard()
+wave = Motion('../../motions/wave.motion')
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+
+    key = keyboard.getKey()
+    
+    
+    if(key==Keyboard.UP):
+       
+       wave.play()
+   
+    pass
+
+# Enter here exit cleanup code.
